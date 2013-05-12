286acdb426c9aad0acd198be3f7824f0840b8ff4
kainage-home <kainage@gmail.com>
2013-05-11 17:35:09 -0700

    added next migration number to generator

--- lib/generators/features_generator.rb
+++ lib/generators/features_generator.rb
@@ -5,6 +5,10 @@ class FeaturesGenerator < Rails::Generators::Base
 	
 	source_root File.expand_path("../templates", __FILE__)
 	
+	def self.next_migration_number(path)
+		Time.now.utc.strftime("%Y%m%d%H%M%S")
+	end
+	
 	def create_featureable_file
 		template "feature.rb", "app/models/feature.rb"
 		migration_template "create_features.rb", "db/migrate/create_features.rb"
